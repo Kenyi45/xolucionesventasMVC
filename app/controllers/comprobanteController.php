08ffc578a6189eba071aca0625fb183b4740ca8b
Isaias Villegas <57301050+IsaiasVillegastm@users.noreply.github.com>
2021-06-15 01:03:06 -0500

    comprobante OK

---
+++ app/controllers/comprobanteController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controllers;
+use Libs\Controller;
+use stdClass;
+
+class ComprobanteController extends Controller
+{
+    public function __construct()
+    {
+        $this->loadDirectoryTemplate('comprobante');
+        $this->loadDAO('comprobante');
+    }
+
+    public function index()
+    {
+        $data = $this->dao->getAll(true);
+        echo $this->template->render('index', ['data' => $data]);
+    }
+
+    public function detail($param=null)
+    {
+        $id = isset($param[0]) ? $param[0] : 0;
+        $data = $this->dao->get($id);
+        echo $this->template->render('detail',['data'=>$data]);
+    }
+
+    public function save()
+    {
+        $obj = new stdClass();
+
+        $obj->IdComprobante = isset($_POST['idcomprobante'])? intval($_POST['idcomprobante']):0;
+        $obj->Nombre = isset($_POST['nombre'])? $_POST['nombre']:'';
+        
+
+        if($obj->IdComprobante>0){
+            $this->dao->update($obj);
+        }else{
+            $this->dao->create($obj);
+        }
+
+        header('Location:' . URL . 'comprobante/index');
+    }
+
+    public function delete($param=null)
+    {
+        $id = isset($param[0]) ? $param[0] : 0;
+        if($id > 0){
+            $this->dao->delete($id);
+        }
+        header('Location:' . URL . 'comprobante/index');
+    }
+}
