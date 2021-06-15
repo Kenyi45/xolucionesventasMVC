cad76953bcc187c5e8162aac8522ded1c0d877fe
Isaias Villegas <57301050+IsaiasVillegastm@users.noreply.github.com>
2021-06-14 21:11:59 -0500

    changes

--- config/config.php
+++ config/config.php
@@ -1,7 +1,12 @@
 <?php
 
-define('URL','http://localhost:82/xolucionesventasMVC/public/');
+define('URL','http://localhost/xolucionesventasMVC/public/');
 // define('URL','http://ivaventas:82/');
 define('ROOT',$_SERVER['DOCUMENT_ROOT']);
 // define('MAINPATH',$_SERVER['DOCUMENT_ROOT']);
 define('MAINPATH', ROOT . '/xolucionesventasMVC/');
+
+define('MUYGRANDE', 'modal-xl');
+define('GRANDE','modal-lg');
+define('NORMAL','');
+define('CHICO', 'modal-sm');
